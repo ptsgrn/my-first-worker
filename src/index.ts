043465a36ebb0b73e980e22856577f077eb7120c
Patsagorn Yuenyong <works@ptsgrn.dev>
2026-03-13 16:20:22 +0700

    ใส่ validation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Elysia } from 'elysia';
+import { Elysia, t } from 'elysia';
 import cors from '@elysiajs/cors';
 import { CloudflareAdapter } from 'elysia/adapter/cloudflare-worker';
 
@@ -16,17 +16,26 @@ export default new Elysia({
 	.use(cors())
 	.get('/', () => 'สวัสดีฉันมาจากประเทศไทย')
 	.get('/tasks', () => tasks)
-	.post('/tasks', ({ body }) => {
-		console.log('Received request body:', body);
-		const { title } = body as { title: string };
-		const newTask: Task = {
-			id: crypto.randomUUID(),
-			title,
-			isCompleted: false,
-		};
+	.post(
+		'/tasks',
+		({ body }) => {
+			console.log('Received request body:', body);
+			const { title } = body as { title: string };
+			const newTask: Task = {
+				id: crypto.randomUUID(),
+				title,
+				isCompleted: false,
+			};
 
-		tasks.push(newTask);
+			tasks.push(newTask);
 
-		return newTask;
-	})
+			return newTask;
+		},
+		{
+			// กำหนด schema สำหรับ body ของ request
+			body: t.Object({
+				title: t.String(),
+			}),
+		},
+	)
 	.compile();
