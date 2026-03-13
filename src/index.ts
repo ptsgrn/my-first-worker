63be11b19f054a8fc55af7846e2262a89ec7521e
Patsagorn Yuenyong <works@ptsgrn.dev>
2026-03-13 14:40:52 +0700

    add scafold in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Elysia } from 'elysia';
+import { CloudflareAdapter } from 'elysia/adapter/cloudflare-worker';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('สวัสดีฉันมาจากประเทศไทย');
-	},
-} satisfies ExportedHandler<Env>;
+export default new Elysia({
+	adapter: CloudflareAdapter,
+})
+	.get('/', () => 'สวัสดีฉันมาจากประเทศไทย')
+	// This is required to make Elysia work on Cloudflare Worker
+	.compile();
